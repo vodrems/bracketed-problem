fb12d4ad324328fe9235b59cd119145a7b062853
Andrew Smerdov <andrew.smerdov@gmail.com>
2018-11-21 20:03:13 +0200

    make correct structure and install composer

---
+++ src/Vodrems/CloseBrackets.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Vodrems;
+
+class CloseBrackets
+{
+    /**
+     * @var string
+     */
+    private $input;
+
+    /**
+     * @var array
+     */
+    private $matchedSymbols = [
+        "(" => 0,
+        ")" => 0,
+        " " => 0,
+        "\n" => 0,
+        "\t" => 0,
+        "\r" => 0,
+    ];
+
+    /**
+     * CloseBrackets constructor.
+     * @param string $inputString
+     */
+    public function __construct(string $inputString)
+    {
+        $this->input = $inputString;
+    }
+
+    /**
+     * @return bool
+     */
+    public function check(): bool
+    {
+        $result = true;
+
+        for ($i = 0; $i < strlen($this->input); $i++) {
+
+            // check if symbol is allowed
+            if (!isset($this->matchedSymbols[$this->input[$i]])) {
+                throw new InvalidArgumentException("Wrong symbol on ${i}\n");
+                break;
+            }
+
+            // check correct numbers of brackets
+            $this->matchedSymbols[$this->input[$i]] += 1;
+
+            if (!$this->checkCountBrackets($this->matchedSymbols, true)) {
+                $result = false;
+                break;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $symbols
+     * @param bool $checkingInProgress
+     * @return bool
+     */
+    private function checkCountBrackets(array $symbols, bool $checkingInProgress = false): bool {
+        $bracket = [
+            'open' => "(",
+            'close' => ")",
+        ];
+
+        if ($checkingInProgress) {
+            if ($symbols[$bracket['open']] < $symbols[$bracket['close']]) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            if ($symbols[$bracket['close']] === $symbols[$bracket['open']]) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
